b53a234b320f7d95a31f6481ba0246ca26f83a8b
dandedotdev <contact@dande.dev>
2025-09-11 14:22:37 +0800

    fix(structs): fix `out_of_bounds` of `BinaryTreeExt`

--- src/structs/binary_tree/implementation.rs
+++ src/structs/binary_tree/implementation.rs
@@ -20,13 +20,13 @@ impl BinaryTreeExt for Option<Rc<RefCell<TreeNode>>> {
         while idx < n && !deque.is_empty() {
             if let Some(node) = deque.pop_front() {
                 let mut node = node.borrow_mut();
-                if values[idx] != i32::MIN {
+                if idx < n && values[idx] != i32::MIN {
                     let node_left = Rc::new(RefCell::new(TreeNode::new(values[idx])));
                     node.left = Some(Rc::clone(&node_left));
                     deque.push_back(node_left);
                 }
                 idx += 1;
-                if values[idx] != i32::MIN {
+                if idx < n && values[idx] != i32::MIN {
                     let node_right = Rc::new(RefCell::new(TreeNode::new(values[idx])));
                     node.right = Some(Rc::clone(&node_right));
                     deque.push_back(node_right);
